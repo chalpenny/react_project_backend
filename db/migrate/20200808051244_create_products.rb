31990d42ff68b14ee34a5a653887f1bb58cee415
chalpenny <cp.halpenny@gmail.com>
2020-08-07 22:39:29 -0700

    set up initial model, controller, migration, & routes

---
+++ db/migrate/20200808051244_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :model
+	    t.string :manufacturer
+	    t.integer :cost_in_credits
+	    t.integer :length
+	    t.integer :max_atmosphering_speed
+	    t.integer :crew
+	    t.integer :passengers
+
+      t.timestamps
+    end
+  end
+end
